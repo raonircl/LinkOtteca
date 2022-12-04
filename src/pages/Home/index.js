4f0b52ec72ecd414d240f67036c9745844a5eb19
raonircl <raoni.afi@gmail.com>
2022-12-04 17:35:21 -0300

    estrutura da pagina home com css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,59 @@
+import './home.css'
 
+import { Logo } from '../../components/Logo'
 
 export default function Home(){
   return(
-    <div>
-      <h1>Página Home!!!!!!!!!!</h1>
+    <div className='home-container'>
+      <Logo/>
+      
+      <span>Meus links</span>
+
+      <main className='links'>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Músicas</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Material de Estudo</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Canais do Youtube</p>
+          </a>          
+        </section>
+
+      </main>
+
+      
+      <main className='links'>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Nóticias</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Instagram</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href="#">
+            <p className='link-text'>Linkedin</p>
+          </a>          
+        </section>
+
+      </main>
+
     </div>
   )
 }
